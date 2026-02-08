8a94012efde8940cb623f60975f0c51bb914838b
96SLM <slmitchell2@pstcc.edu>
2026-02-08 12:29:25 -0500

    Addions: Commented out starting code, Main function changes -> new surface with modifier and colorscheme, call the profiletext funcion, test information used ProfileText function changes -> text alignment codes added, text sizes and padding added Unknown change to deploymentTargetSelector.xml

--- app/src/main/java/com/example/profilecard2026/MainActivity.kt
+++ app/src/main/java/com/example/profilecard2026/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.example.profilecard2026
 
+import android.R.attr.lineHeight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,28 +30,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProfileCard2026Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   //TODO: change to final Profile Card
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                   //TODO: change to final Profile Card
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    ProfileText(
+                        "Daniel Andrews",
+                        "Test \nTest \nTest",
+                        modifier = Modifier.padding(8.dp)
                     )
                 }
             }
         }
     }
 }
-//TODO: delete from final project
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.LightGray) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-                .padding(16.dp)
-        )
-    }
-}
+////TODO: delete from final project
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Surface(color = Color.LightGray) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//                .padding(16.dp)
+//        )
+//    }
+//}
 //Function to contain and style other functions
 //TODO: add style to box, arrange elements of profile card, create preview
 @Composable
@@ -67,34 +82,43 @@ fun ProfileImage(){
 //TODO: add text, style text, add alternate text feature
 @Composable
 fun ProfileText(name: String, bio: String, modifier: Modifier = Modifier){
-    Column(modifier = modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
         Text(
             text = "$name",
-            fontSize = 15.sp,
-            lineHeight = 20.sp
+            fontSize = 52.sp,
+            lineHeight = 60.sp,
+            textAlign = TextAlign.Start
         )
         Text(
             text  = "$bio",
-            fontSize = 5.sp,
-            lineHeight = 10.sp
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.Start)
         )
     }
   
 
 }
 
-//TODO: change or delete
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProfileCard2026Theme {
-        Greeting("Android")
-    }
-}
+////TODO: change or delete
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ProfileCard2026Theme {
+//        Greeting("Android")
+//    }
+//}
 @Preview
 @Composable
 fun ProfileTextPreview(){
     ProfileCard2026Theme {
-        ProfileText("Daniel Andrews", "Test Test Test")
+        ProfileText(
+            "Daniel Andrews",
+            "Test \nTest \nTest"
+        )
     }
 }
