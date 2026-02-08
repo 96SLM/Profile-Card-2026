4bec84ffa5ac699194e910daeffd364cfcd56962
96SLM <slmitchell2@pstcc.edu>
2026-02-08 11:54:29 -0500

    Addions: Changed Starting code -> added surface container, added padding modifier Changed ProfileInfo name -> new name ProfileText, surrounded text elements with column, added fontsize and linehight to both text elements Added ProfileTextPreview -> tested linehight and fontsize, tested name and bio parameteres

--- app/src/main/java/com/example/profilecard2026/MainActivity.kt
+++ app/src/main/java/com/example/profilecard2026/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.profilecard2026.ui.theme.ProfileCard2026Theme
 
 class MainActivity : ComponentActivity() {
@@ -34,10 +39,13 @@ class MainActivity : ComponentActivity() {
 //TODO: delete from final project
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.LightGray) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+                .padding(16.dp)
+        )
+    }
 }
 //Function to contain and style other functions
 //TODO: add style to box, arrange elements of profile card, create preview
@@ -46,7 +54,7 @@ fun ProfileCard(){
     Box{
        ProfileImage()
 
-//       ProfileInfo()
+//       ProfileText()
     }
 }
 //function to contain and style image
@@ -58,15 +66,20 @@ fun ProfileImage(){
 //function to contain and style text
 //TODO: add text, style text, add alternate text feature
 @Composable
-fun ProfileInfo(name: String, bio: String, modifier: Modifier = Modifier){
-    Text(
-        text = "$name",
-        modifier = modifier
-    )
-    Text(
-        text  = "$bio",
-        modifier = modifier
-    )
+fun ProfileText(name: String, bio: String, modifier: Modifier = Modifier){
+    Column(modifier = modifier) {
+        Text(
+            text = "$name",
+            fontSize = 15.sp,
+            lineHeight = 20.sp
+        )
+        Text(
+            text  = "$bio",
+            fontSize = 5.sp,
+            lineHeight = 10.sp
+        )
+    }
+  
 
 }
 
@@ -76,5 +89,12 @@ fun ProfileInfo(name: String, bio: String, modifier: Modifier = Modifier){
 fun GreetingPreview() {
     ProfileCard2026Theme {
         Greeting("Android")
+    }
+}
+@Preview
+@Composable
+fun ProfileTextPreview(){
+    ProfileCard2026Theme {
+        ProfileText("Daniel Andrews", "Test Test Test")
     }
 }
